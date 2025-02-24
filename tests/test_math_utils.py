87e1d287352c15c81fa98d896ab31251bb7bc63d
rnewso4 <54243809+rnewso4@users.noreply.github.com>
2025-02-24 16:01:57 -0600

    adding comments

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -3,23 +3,27 @@ sys.path.append('../PYTHON-BAZEL-CI')
 from src.math_utils import *
 
 def test_add():
+    '''Some methods for testing the add function'''
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(-1, -1) == -2
 
 def test_subtract():
+    '''Some methods for testing the subtract function'''
     assert subtract(3, 2) == 1
     assert subtract(-1, 1) == -2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
 
 def test_multiply():
+    '''Some methods for testing the multiply function'''
     assert multiply(3, 2) == 6
     assert multiply(-1, 1) == -1
     assert multiply(0, 0) == 0
     assert multiply(-1, -1) == 1
 
 def test_divide():
+    '''Some methods for testing the divide function'''
     assert divide(4, 2) == 2
     assert divide(-1, 1) == -1
     assert divide(0, 1) == 0
--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -3,6 +3,7 @@ sys.path.append('../PYTHON-BAZEL-CI')
 from src.string_utils import *
 
 def test_reverse_string():
+    '''Some methods for testing the reverse string function'''
     assert reverse_string("hello") == "olleh"
     assert reverse_string("world") == "dlrow"
     assert reverse_string("") == ""
@@ -10,12 +11,14 @@ def test_reverse_string():
     assert reverse_string("12345") == "54321"
 
 def test_is_palindrome():
+    '''Some methods for testing the palindrome function'''
     assert is_palindrome("racecar") == True
     assert is_palindrome("hello") == False
     assert is_palindrome("a") == True
     assert is_palindrome("") == True
 
 def test_count_vowels():
+    '''Some methods for testing the count vowels function'''
     assert count_vowels("racecar") == 3
     assert count_vowels("world") == 1
     assert count_vowels("") == 0
